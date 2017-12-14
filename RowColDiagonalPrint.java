a735b8b01d911608743e3daac1f6e582aa5cfedd
Parshuram reddy <pandu@Parshurams-MacBook-Pro.local>
2017-12-14 07:04:24 +0530

    added class name

---
+++ RowColDiagonalPrint.java
@@ -0,0 +1,13 @@
+class RowColDiagonalPrint{
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+        RowColDigonalPrint instance = new RowColDigonalPrint();
+        instance.printMatrix();
+    }
+    /*
+     *
+     */
+    void printMatrix() {
+
+    }
+}
