fa60a1e1535a7e563893968223a5b56ae693a618
Parshuramreddy <parshuram.sudda@gmail.com>
2018-05-02 17:34:56 +0530

    AmazonAdded

--- RowColDiagonalPrint.java
+++ RowColDiagonalPrint.java
@@ -1,37 +1,119 @@
 class RowColDiagonalPrint {
-    public void printMatrix() {
-        int size = 5, rowIndex, colIndex;
-        int[][] a = new int[size][size];
-        for (rowIndex = 0; rowIndex <= size - 1; rowIndex++) {
+    int grid[][];
 
-            for (colIndex = 0; colIndex <= size - 1; colIndex++) {
-                /*
-                 * first print the first row and first columni==0,colIndex==0
-                 * print last row and last coloumn rowIndex=size-1,colIndex==size-1
-                 * print principal digonal elemnts rowIndex==colIndex
-                 * print opposite digonal rowIndex+colIndex==ize-1
-                 */
-                if (rowIndex == 0 || colIndex == 0 ||
-                        rowIndex == size-1 || colIndex == size-1 ||
-                        rowIndex==colIndex ||
-                        rowIndex + colIndex == size-1)
-                {
-                    System.out.print(a[rowIndex][colIndex] + " ");
+    public RowColDiagonalPrint() {
+        grid[][] = {
+            {1,2,3,4,5},
+            {11,12,13,14,15},
+            {21,22,23,24,25},
+            {31,32,33,34,35},
+            {21,22,23,24,25}
+        };
+    }
 
-                }
-                else {
-                    System.out.print("  ");
-                }
+    //printing first column
+//    public void printBorder(){
+//        int size=5,i,j;
+//        //int [][] a = new int[size][size];
+//        int [][] a= { {1, 2,3} , { 4, 5,6},{7,8,9} };
+//        for(i=0;i<=size-1;i++)
+//        {
+//            System.out.println(a[i][0] + " ");
+//        }
+//    }
 
+    //printing last row
+    public void printLastRow() {
+        int size = 5, i, j,f=0;
+        int[][] grid = new int[size][size];
+        for (i = 1; i <= size - 1; i++)
+        {
+            System.out.println(grid[size-1][i] + " ");
+            f++;
+        }
+        System.out.println("no of times is "+f);
+    }
+    //printing last column
+    public void printLastColumn() {
+        int size = 5, i, j;
+        int[][] grid = new int[size][size];
+        for (i = size - 2; i > 0; i--) {
+            System.out.println(grid[i][size - 1] + " ");
+//            System.out.println("test");
+        }
+    }
+    //printing first row
+        public void printBorder4(){
+            int size=5,i,j;
+            int [][] grid = new int[size][size];
+            for(i=size-2;i> 0;i--)
+            {
+                System.out.println(grid[size-1][i] + " ");
+//                System.out.println("test");
             }
-            System.out.println();
+
+    }
+
+   //printing second column second row
+    public void printBorder12() {
+        int size = 5, i, j;
+        int[][] grid = new int[size][size];
+        for (i = 1; i <= size - 2; i++)
+        {
+            System.out.println(grid[i][size-4] + " ");
+        }
+    }
+    //printing 4th row 4th column
+    public void printBorder21() {
+        int size = 5, i, j;
+        int[][] grid = new int[size][size];
+        for (i = 1; i <= size - 2; i++)
+        {
+            System.out.println(grid[size-2][i] + " ");
+        }
+    }
+//printing 3rd row and 3rd column
+    public void printBorder31() {
+        int size = 5, i, j;
+        int[][] grid = new int[size][size];
+        for (i = 3; i <= size - 4; i--)
+        {
+            System.out.println(grid[i][size-2]+ " ");
+        }
+    }
+    //printing 2nd row 2nd column
+
+    public void printBorder32() {
+        int size = 5, i, j;
+        int[][] grid = new int[size][size];
+        for (i = size-2; i <= size - 3; i--)
+        {
+            System.out.println(grid[size-4][i]+ " ");
+        }
+    }
+//printing middle elemnt
+    public void printBorder22() {
+        int size = 5, i, j;
+        int[][] grid = new int[size][size];
+        for (i = size-3; i <= size - 3; i++)
+        {
+            System.out.println(grid[size-3][size-3]+ " ");
         }
     }
 
     public static void main(String args[]) {
         System.out.println("Hello Java");
+        int f=1;
         RowColDiagonalPrint rowColDiagonalPrint = new RowColDiagonalPrint();
         rowColDiagonalPrint.printMatrix();
+//        rowColDiagonalPrint.printBorder();
+        rowColDiagonalPrint.printLastRow();
+        rowColDiagonalPrint.printLastColumn();
+        rowColDiagonalPrint.printBorder4();
+        rowColDiagonalPrint.printBorder12();
+        rowColDiagonalPrint.printBorder21();
+        rowColDiagonalPrint.printBorder31();
+        rowColDiagonalPrint.printBorder32();
+        rowColDiagonalPrint.printBorder22();
     }
-
 }
