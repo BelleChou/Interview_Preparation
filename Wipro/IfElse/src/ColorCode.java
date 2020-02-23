ceb0d0aa11691d2cc99622cf35eb33e92104f75b
Parshuarm Reddy <parshuram.sudda@gmail.com>
2020-02-23 12:39:35 +0530

    Month Program Updated

--- Wipro/IfElse/src/ColorCode.java
+++ Wipro/IfElse/src/ColorCode.java
@@ -2,13 +2,24 @@ import java.util.Scanner;
 
 public class ColorCode {
 	public static void main (String args[]) {
+		@SuppressWarnings("resource")
 		Scanner reader = new Scanner(System.in);
 		char c = reader.next().charAt(0);
-		System.out.print(c);
+		switch(c) {
+		case 'R':System.out.print("Red");
+		break;
+		case 'B':System.out.print("Blue");
+		break;
+		case 'G':System.out.print("Green");
+		break;
+		case 'O':System.out.print("Orange");
+		break;
+		case 'Y':System.out.print("Yellow");
+		break;
+		default:System.out.print("Invalid Code");
+		}
 	}
 
 }
 
 
-
-//R->Red, B->Blue, G->Green, O->Orange, Y->Yellow, W->White
