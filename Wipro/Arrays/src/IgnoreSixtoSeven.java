0c603028232c2180605849ccce9acf3d78d3df2c
Parshuarm Reddy <parshuram.sudda@gmail.com>
2020-02-27 15:42:17 +0530

    First Project Updated

---
+++ Wipro/Arrays/src/IgnoreSixtoSeven.java
@@ -0,0 +1,26 @@
+/**
+ Write a program to print the sum of the elements of an array following the given below condition.
+
+If the array has 6 and 7 in succeeding orders, ignore the numbers between 6 and 7 and consider the other numbers for calculation of sum.
+
+Eg1) Array Elements - 10,3,6,1,2,7,9
+O/P: 22   
+[i.e 10+3+9]
+ * 
+ */
+
+/**
+ * @author parsh
+ *
+ */
+public class IgnoreSixtoSeven {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
