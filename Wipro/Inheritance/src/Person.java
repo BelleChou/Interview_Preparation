d3509ae2df15525e270d3e3882a378ba0a2f93b3
Parshuarm Reddy <parshuram.sudda@gmail.com>
2020-03-26 21:10:27 +0530

    Inheritance Added

--- Wipro/Inheritance/src/Person.java
+++ Wipro/Inheritance/src/Person.java
@@ -12,17 +12,16 @@ public class Person {
 	 * 
 	 */
 	 
-	String name,dateOfBirth;
+	String name;
+	static String dateOfBirth;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		Teacher t=new Teacher(234);
-		Student s=new Student("Wipro", 1920);
-		
-
+		Teacher t=new Teacher(24000,"English");
+		CollegeStudent c=new CollegeStudent("SRi Indu ","2020", "16D41A05J6");
 	}
 
 }
@@ -31,22 +30,35 @@ public class Person {
 		int salary;
 		String subject;
 		
-	 public Teacher(int salary){
+	 public Teacher(int salary,String subject){
 		 this.salary=salary;
-		 System.out.println("Salary is"+this.salary);
+		 this.subject=subject;
+		 System.out.println("Salary is"+this.salary+" Subject is "+this.subject);
 	 }
 	
 	 
 
 }
  class Student extends Person{
-	 String collegeName;
-	 int year;
-	 public Student(String collegeName, int year)
+	 String studentID;
+	 public Student(String studentID)
 	 {
-		 this.year=year;
-		 this.collegeName=collegeName;
-		 System.out.println("Year is "+this.year+" College is "+this.collegeName);
+		 this.studentID=studentID;
+		 System.out.println("StudentID is "+this.studentID);
 	 }
+	 
+ }
+ class  CollegeStudent extends Student {
+
+	private String collegeName;
+	private String year;
+
+	public CollegeStudent(String collegeName, String year, String StudentID) {
+		 super(StudentID);
+		 this.collegeName=collegeName;
+		 this.year=year;
+		 System.out.println("Year is "+this.collegeName+" College is "+this.studentID);
+		// TODO Auto-generated constructor stub
+	}
 	 
  }
