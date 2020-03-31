8a2b67e42926b0c5ebc762018fbd44e94b18a720
Parshuarm Reddy <parshuram.sudda@gmail.com>
2020-03-31 21:14:31 +0530

    Assignment Pushed

--- Wipro/ArrayList/src/Assignment5.java
+++ Wipro/ArrayList/src/Assignment5.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * 
  */
@@ -8,12 +11,30 @@
  */
 public class Assignment5 {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+	
+public static void main(String[] args) {
+	LinkedList<String> list = new LinkedList<String>();
+	list.add("January");
+	list.add("February");
+	list.add("March");
+	list.add("April");
+	list.add("May");
+	list.add("June");
+	list.add("July");
+	list.add("August");
+	list.add("September");
+	list.add("October");
+	list.add("November");
+	list.add("Decemberem");
 
+	printAll(list);
+}
+
+public static void printAll(LinkedList<String> list) {
+	Iterator<String> it = list.iterator();
+	
+	while (it.hasNext())
+		System.out.println(it.next());
+}
 }
